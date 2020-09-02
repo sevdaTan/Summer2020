0aa316c3ba9d01f8a2c3338a41c64b2ad74c19fc
sevdaTan <sevdatanyildizi@gmail.com>
2020-09-02 18:18:31 -0400

    Office hour 09_02_2020

--- src/Office_Hours/Practice_09_02_2020/Circle.java
+++ src/Office_Hours/Practice_09_02_2020/Circle.java
@@ -3,9 +3,13 @@ package Office_Hours.Practice_09_02_2020;
 public class Circle {
     public double radius;
     public double diameter;
-    public static double PI = 3.14;
+    public static double PI ;
     public double area;
     public double perimeter;
+
+    static { // only runs one time, first to run
+        PI = 3.14;
+    }
 
     public Circle(double radius){
         this.radius = radius;
