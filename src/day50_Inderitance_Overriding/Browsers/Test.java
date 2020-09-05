89b179de5b0eba27655e3f1f9945477eed3fdb29
sevdaTan <sevdatanyildizi@gmail.com>
2020-09-05 10:36:01 -0400

    day50

--- src/day50_Inderitance_Overriding/Browsers/Test.java
+++ src/day50_Inderitance_Overriding/Browsers/Test.java
@@ -6,21 +6,21 @@ package day50_Inderitance_Overriding.Browsers;
 public class Test {
     public static void main(String[] args) {
 
-         ChromeBrowser obj1 = new ChromeBrowser();
-         obj1.get("www.google.com");
-         obj1.close();
+         ChromeBrowser chrome = new ChromeBrowser();
+         chrome.get("www.google.com");
+         chrome.close();
 
         System.out.println("============");
 
-         FirefoxBrowser obj2 = new FirefoxBrowser();
-         obj2.get("www.amazon.com");
-         obj2.close();
+         FirefoxBrowser firefox = new FirefoxBrowser();
+         firefox.get("www.amazon.com");
+         firefox.close();
 
         System.out.println("===========");
 
-        OperaBrowser obj3 = new OperaBrowser();
-        obj3.get("www.facebook.com");
-        obj3.close();
+        OperaBrowser opera = new OperaBrowser();
+        opera.get("www.facebook.com");
+        opera.close();
 
 
     }
